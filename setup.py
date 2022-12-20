e69d9f117fd29eb8cf7af3d526bd8cbda9dfe2c5
Colin <wode19930724@163.com>
2022-12-20 10:13:18 +0800

    1
    Signed-off-by: Colin <wode19930724@163.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
       name='colin-qinglong', #项目名称，取值随意
-      version='1.0.0', #版本号
+      version='1.0.1', #版本号
       py_modules=['myQLApi'], #对应模块文件的名称
       author='colin',
       author_email='colin@163.com',
